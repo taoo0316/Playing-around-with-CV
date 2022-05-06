30cfbdb46fa84760ee0f8dbd1217d25066a3cdfd
Zhu Wentao <95064358+taoo0316@users.noreply.github.com>
2022-05-07 03:37:54 +0800

    Update read_images_and_videos_opencv.py

--- read_images_and_videos_opencv.py
+++ read_images_and_videos_opencv.py
@@ -1,42 +1,33 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+
 
 
 import cv2 as cv
 
-
-# In[2]:
-
+# read images
 
 img_1 = cv.imread("/Users/zwt2000/Desktop/cute/timg.jpeg")
-
-
-# In[ ]:
-
 
 cv.imshow("Dream",img_1)
 cv.waitKey(0)
 
+#read videos
 
-# In[ ]:
+capture = cv.VideoCapture("/Users/zwt2000/Downloads/Several Days Later _ SpongeBob Time Card #36.mp4")
 
-
-#capture = cv.VideoCapture("/Users/zwt2000/Downloads/Several Days Later _ SpongeBob Time Card #36.mp4")
-
-#while True:
-    #isTrue, frame = capture.read()
-    #cv.imshow("Video", frame)
+while True:
+    isTrue, frame = capture.read()
+    cv.imshow("Video", frame)
     
-    #if cv.waitKey(20) & 0xFF==ord("d"):
-        #break
+    if cv.waitKey(20) & 0xFF==ord("d"):
+        break
         
-#capture.realease()
-#cv.destroyAllWindows()
+capture.realease()
+cv.destroyAllWindows()
 
 
-# In[ ]:
 
 
 
